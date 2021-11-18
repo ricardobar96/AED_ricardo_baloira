a0ea52f107467d185bc9e4704ac96094f8d01749
Ricardo Baloira <ricardobar96@gmail.com>
2021-11-18 11:48:09 +0000

    Subida práctica del Tema 3 consistente en persistir un objeto Moneda y guardarlo en la base de datos seguimientomonedas

---
+++ Tema 3/practica_persistirMoneda_AED_ricardo_baloira/src/main/java/es/iespuertodelacruz/ricardo/persistirMoneda/entities/Usuario.java
@@ -0,0 +1,78 @@
+package es.iespuertodelacruz.ricardo.persistirMoneda.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the usuario database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Usuario.findAll", query="SELECT u FROM Usuario u")
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idusuario;
+
+	private String nombre;
+
+	private String password;
+
+	//bi-directional many-to-one association to UsuarioRol
+	@OneToMany(mappedBy="usuario")
+	private List<UsuarioRol> usuarioRols;
+
+	public Usuario() {
+	}
+
+	public int getIdusuario() {
+		return this.idusuario;
+	}
+
+	public void setIdusuario(int idusuario) {
+		this.idusuario = idusuario;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<UsuarioRol> getUsuarioRols() {
+		return this.usuarioRols;
+	}
+
+	public void setUsuarioRols(List<UsuarioRol> usuarioRols) {
+		this.usuarioRols = usuarioRols;
+	}
+
+	public UsuarioRol addUsuarioRol(UsuarioRol usuarioRol) {
+		getUsuarioRols().add(usuarioRol);
+		usuarioRol.setUsuario(this);
+
+		return usuarioRol;
+	}
+
+	public UsuarioRol removeUsuarioRol(UsuarioRol usuarioRol) {
+		getUsuarioRols().remove(usuarioRol);
+		usuarioRol.setUsuario(null);
+
+		return usuarioRol;
+	}
+
+}
