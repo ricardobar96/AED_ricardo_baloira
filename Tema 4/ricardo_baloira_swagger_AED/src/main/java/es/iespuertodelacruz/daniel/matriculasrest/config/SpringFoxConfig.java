1ddd3e247444c452af24be9b70423644cb0ccaa2
Ricardo Baloira <ricardobar96@gmail.com>
2022-01-09 23:14:31 +0000

    Actualizadas anotaciones del proyecto comentado con Swagger

--- Tema 4/ricardo_baloira_swagger_AED/src/main/java/es/iespuertodelacruz/daniel/matriculasrest/config/SpringFoxConfig.java
+++ Tema 4/ricardo_baloira_swagger_AED/src/main/java/es/iespuertodelacruz/daniel/matriculasrest/config/SpringFoxConfig.java
@@ -20,7 +20,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
-	
 	@Primary
 	@Bean
     public LinkDiscoverers discoverers() {
