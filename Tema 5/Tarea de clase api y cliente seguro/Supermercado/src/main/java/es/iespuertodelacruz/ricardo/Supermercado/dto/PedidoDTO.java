808302315dab6f242a61e1712f69a223491781ee
Ricardo Baloira <ricardobar96@gmail.com>
2022-02-16 12:34:41 +0000

    Actualizada tarea API supermercado (creados clases de los paquetes services, dto, security y repositories; creado controller login)

---
+++ Tema 5/Tarea de clase api y cliente seguro/Supermercado/src/main/java/es/iespuertodelacruz/ricardo/Supermercado/dto/PedidoDTO.java
@@ -0,0 +1,95 @@
+package es.iespuertodelacruz.ricardo.Supermercado.dto;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import es.iespuertodelacruz.ricardo.Supermercado.entities.Detallepedido;
+import es.iespuertodelacruz.ricardo.Supermercado.entities.Pedido;
+
+public class PedidoDTO {
+	private int idpedido;
+	
+	private String direccionEntrega;
+
+	private byte entregado;
+
+	private byte enviado;
+
+	private BigInteger fecha;
+
+	private byte pagado;
+	
+	private List<Detallepedido> detallepedidos;
+
+	public PedidoDTO() {
+		super();
+	}
+
+	public PedidoDTO(Pedido p) {
+		super();
+		this.idpedido = p.getIdpedido();
+		this.direccionEntrega = p.getDireccionEntrega();
+		this.entregado = p.getEntregado();
+		this.enviado = p.getEnviado();
+		this.fecha = p.getFecha();
+		this.pagado = p.getPagado();
+		this.detallepedidos = p.getDetallepedidos();
+	}
+
+	public int getIdpedido() {
+		return idpedido;
+	}
+
+	public void setIdpedido(int idpedido) {
+		this.idpedido = idpedido;
+	}
+
+	public String getDireccionEntrega() {
+		return direccionEntrega;
+	}
+
+	public void setDireccionEntrega(String direccionEntrega) {
+		this.direccionEntrega = direccionEntrega;
+	}
+
+	public byte getEntregado() {
+		return entregado;
+	}
+
+	public void setEntregado(byte entregado) {
+		this.entregado = entregado;
+	}
+
+	public byte getEnviado() {
+		return enviado;
+	}
+
+	public void setEnviado(byte enviado) {
+		this.enviado = enviado;
+	}
+
+	public BigInteger getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(BigInteger fecha) {
+		this.fecha = fecha;
+	}
+
+	public byte getPagado() {
+		return pagado;
+	}
+
+	public void setPagado(byte pagado) {
+		this.pagado = pagado;
+	}
+
+	public List<Detallepedido> getDetallepedidos() {
+		return detallepedidos;
+	}
+
+	public void setDetallepedidos(List<Detallepedido> detallepedidos) {
+		this.detallepedidos = detallepedidos;
+	}
+
+}
